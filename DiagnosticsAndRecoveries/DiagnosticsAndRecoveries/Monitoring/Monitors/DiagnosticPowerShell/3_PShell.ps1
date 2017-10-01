c936a75f94e57f4794bfac4a9c2f681405bcb322
Graham Davies <coding@f1point2.com>
2017-10-01 15:38:13 +0100

    Update

---
+++ DiagnosticsAndRecoveries/DiagnosticsAndRecoveries/Monitoring/Monitors/DiagnosticPowerShell/3_PShell.ps1
@@ -0,0 +1,5 @@
+﻿if ([System.Diagnostics.EventLog]::SourceExists("Diagnostic PowerShell") -eq $False) {
+    New-EventLog -LogName "Visual Studio Builds" -Source "Diagnostic PowerShell"
+	}
+
+Write-EventLog -LogName Application -Source "Diagnostic PowerShell" -EntryType Information -EventID 1234 -Message "Diagnostic Has Completed"
