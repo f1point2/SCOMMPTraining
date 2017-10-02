0a15faa02abbea10824d38cb4500cdd24c8005f8
Graham Davies <coding@f1point2.com>
2017-10-02 21:26:19 +0100

    Updates

---
+++ PShellFileContentsCheck2State/PShellFileContentsCheck2State/Monitoring/Monitors/FileCheck2State/1_PShellScript.ps1
@@ -0,0 +1,27 @@
+﻿
+$Path = "C:\LogFileApp\Output.txt"
+
+#Setup the objects
+$api=New-Object -comObject 'MOM.ScriptAPI'
+$bag=$api.CreatePropertyBag()
+
+if(![System.IO.File]::Exists($path)){
+    $bag.AddValue('FileContent','NoFile')
+}
+
+Else 
+
+{
+
+$api.LogScriptEvent('Check File Contents = ',40,0,$Value)
+$Value = Get-Content $Path
+$bag.AddValue('FileContent',$Value)
+
+}
+
+#Return property bag
+$bag
+
+# Testing
+# $api.Return($bag)
+
