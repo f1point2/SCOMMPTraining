e9b94052eff53c0ec49c3bdcd4bf96edfb62a9a2
Graham Davies <coding@f1point2.com>
2017-10-05 17:23:14 +0100

    New

--- F12.FTPMonitoring/F12.FTPMonitoring/Monitoring/Monitors/FTPMonitor/2_FTPCheck.ps1
+++ F12.FTPMonitoring/F12.FTPMonitoring/Monitoring/Monitors/FTPMonitor/2_FTPCheck.ps1
@@ -1,18 +1,29 @@
 ﻿param($FTPPath)
 
+
+
+
+
 $api=new-Object -comObject "MOM.ScriptAPI"
 $bag= $api.CreatePropertyBag()
- 
+
 $Request=[System.Net.WebRequest]::Create($FTPPath)
  
 $Request.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectoryDetails
  
-$Request.Credentials = New-Object System.Net.NetworkCredential $RunAs[Name="F12.FTPMonitoring.RunAsProfile"]/UserName$, $RunAs[Name="F12.FTPMonitoring.RunAsProfile"]/Password$
- 
+$Request.Credentials = New-Object System.Net.NetworkCredential $RunAs[Name="F12.FTPMonitoring.RunAsProfile"]/UserName$, $RunAs[Name="F12.FTPMonitoring.RunAsProfile"]/Password$, $RunAs[Name="F12.FTPMonitoring.RunAsProfile"]/Domain$
+
+# $Request.Credentials = New-Object System.Net.NetworkCredential::DefaultNetworkCredentials
+
+$api.logscriptevent("FTPCheck.ps1",999,0, "Script Run") 
+
 $Request.KeepAlive=0
  
 $Request.UseBinary=1
  
+Try
+{ 
+
 $Response = $Request.GetResponse()
  
 $ResponseStream=$Response.GetResponseStream()
@@ -22,6 +33,14 @@ $Reader = New-Object System.Io.StreamReader $ResponseStream
 [System.Console]::Writeline($Reader.ReadToEnd())
  
 $bag.AddValue("StatusDescription", $Response.StatusDescription)
+
+}
+
+Catch
+{ 
+$ErrorMessage = $_.Exception.Message
+$bag.AddValue("StatusDescription", $ErrorMessage)
+}
 
 $bag
 
