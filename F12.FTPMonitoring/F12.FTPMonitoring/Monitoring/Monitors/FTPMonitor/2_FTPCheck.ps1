d658fa09ed7503651e9afdeb0176611aa7d629c2
Graham Davies <coding@f1point2.com>
2017-10-05 14:00:27 +0100

    New

---
+++ F12.FTPMonitoring/F12.FTPMonitoring/Monitoring/Monitors/FTPMonitor/2_FTPCheck.ps1
@@ -0,0 +1,29 @@
+﻿param($FTPPath)
+
+$api=new-Object -comObject "MOM.ScriptAPI"
+$bag= $api.CreatePropertyBag()
+ 
+$Request=[System.Net.WebRequest]::Create($FTPPath)
+ 
+$Request.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectoryDetails
+ 
+$Request.Credentials = New-Object System.Net.NetworkCredential $RunAs[Name="F12.FTPMonitoring.RunAsProfile"]/UserName$, $RunAs[Name="F12.FTPMonitoring.RunAsProfile"]/Password$
+ 
+$Request.KeepAlive=0
+ 
+$Request.UseBinary=1
+ 
+$Response = $Request.GetResponse()
+ 
+$ResponseStream=$Response.GetResponseStream()
+ 
+$Reader = New-Object System.Io.StreamReader $ResponseStream
+ 
+[System.Console]::Writeline($Reader.ReadToEnd())
+ 
+$bag.AddValue("StatusDescription", $Response.StatusDescription)
+
+$bag
+
+$Reader.Close()
+$Response.Close()
